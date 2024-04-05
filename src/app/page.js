0c7284b7da10d5be68090dd3203b714f433d78cc
Quique26 <luis.nunez@cuidum.com>
2024-04-05 11:00:43 +0200

    nextjs-uploading form

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,23 @@
+"use client"
+import { useState } from "react"
+
 function HomePage() {
+
+  const [file, setFile] = useState(null)
+
   return (
     <div>
-      HomePage
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        
+        if(!file) return
+
+        console.log('uploading file')
+      }}>
+        <label>Upload file:</label>
+        <input type="file" onChange={(e) => {setFile(e.target.files[0])}}/>
+        <button>Upload</button>
+      </form>
     </div>
   )
 }
